2b3eebbd1129a25aed03c3a8b4401dbe67b84d23
Matteo Bisardi <matteo.bisardi@gmail.com>
2021-11-19 14:42:30 +0100

    Bug fixed

--- src/read_write_MSA.jl
+++ src/read_write_MSA.jl
@@ -74,10 +74,8 @@ function fasta2matrix(filename::AbstractString; max_gap_fraction = 1)
     @assert seqid == length(seqs) + 1
 
     close(f)
-    @show size(Z)
-    println("ciaooo")
     size(Z)[2] == 1 && return Z[:, 1]
-    return 0#Z'
+    return Z'
 end
 
 
